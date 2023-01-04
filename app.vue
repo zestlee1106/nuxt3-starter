ff040852b1b034cb7fd0c41093961e0681e13f47
Youngwoo <zestlee1106@gmail.com>
2023-01-04 11:10:43 +0900

    [feat] tailwind 추가

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="flex justify-center items-center h-screen">
+    <h1 class="text-3xl font-bold text-green-500">
+      How to install Tailwind CSS 3 in Nuxt 3
+    </h1>
   </div>
 </template>
