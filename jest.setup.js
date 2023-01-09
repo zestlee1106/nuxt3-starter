e8c6002bee420a28949b2841b4eb6e099350b41d
Youngwoo <zestlee1106@gmail.com>
2023-01-09 11:54:01 +0900

    🔧 [test] 테스트 셋업 수정
    - NuxtLink stub 에 RouterLinkStub 를 연동

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
 import VueTestUtils from "@vue/test-utils";
 
 // Mock Nuxt components
-VueTestUtils.config.global.stubs["NuxtLink"] = "<a><slot /></a>";
+VueTestUtils.config.global.stubs["NuxtLink"] = VueTestUtils.RouterLinkStub;
