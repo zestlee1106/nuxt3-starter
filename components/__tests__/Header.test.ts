07c8c211c9677d283d7a85159c4e50412b198e81
Youngwoo <zestlee1106@gmail.com>
2023-01-12 22:20:13 +0900

    🧪 [test] 실패 테스트 추가

--- components/__tests__/Header.test.ts
+++ components/__tests__/Header.test.ts
@@ -20,4 +20,47 @@ describe("Header 테스트", () => {
       expect(HomeNuxtLink.props("to")).toEqual("/");
     });
   });
+
+  describe("사용자는 테마를 바꿀 수 있다.", () => {
+    const IS_DARK_MODE = true;
+
+    it("아무것도 하지 않았다면, 테마는 다크모드 상태이다.", () => {
+      const isDark = wrapper.vm.isDark;
+
+      expect(isDark).toEqual(IS_DARK_MODE);
+    });
+
+    it("테마 바꾸기 버튼을 1번 누르면, 테마 바꾸는 함수를 1번 호출한다.", async () => {
+      const changeThemeButton = wrapper.findComponent(
+        '[data-testid="changeThemeButton"]'
+      );
+      const changeTheme = jest.spyOn(wrapper.vm, "changeTheme");
+
+      await changeThemeButton.trigger("click");
+
+      expect(changeTheme).toBeCalledTimes(1);
+
+      wrapper.vm.$destroy();
+    });
+
+    it("테마 바꾸는 함수를 호출하면, 테마 상태가 바뀐다.", () => {
+      wrapper.vm.changeTheme();
+
+      expect(wrapper.vm.isDark).toEqual(!IS_DARK_MODE);
+
+      wrapper.vm.$destroy();
+    });
+
+    it("테마 바꾸는 함수를 2번 호출하면, 테마 상태가 원래대로 돌아온다", () => {
+      const changeTheme = jest.spyOn(wrapper.vm, "changeTheme");
+
+      wrapper.vm.changeTheme();
+      wrapper.vm.changeTheme();
+
+      expect(changeTheme).toBeCalledTimes(2);
+      expect(wrapper.vm.isDark).toEqual(IS_DARK_MODE);
+
+      wrapper.vm.$destroy();
+    });
+  });
 });
