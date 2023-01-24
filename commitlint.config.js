450c998170f8f32fa43a98edeec3d4d8890ad468
Youngwoo <zestlee1106@gmail.com>
2023-01-24 09:20:10 +0900

    [feat] 커밋 린트 수정

--- commitlint.config.js
+++ commitlint.config.js
@@ -7,10 +7,8 @@ module.exports = {
       rules: {
         'required-issue-number': ({ header: commitMsg }) => {
           const regCommitSubject = /\[.*?\]/
-          const regIssueNumber = /\w{3,4}-\d+/g
-          if (regCommitSubject.test(commitMsg) && regIssueNumber.test(commitMsg))
-            return [true, '커밋 성공!']
-          else return [false, '이슈 번호를 포함해야 합니다.']
+          if (regCommitSubject.test(commitMsg)) return [true, '커밋 성공!']
+          else return [false, '[] 를 포함해서 컨벤션을 맞춰야 합니다.']
         },
       },
     },
