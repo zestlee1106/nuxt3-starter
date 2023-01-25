bea60b232dae6e14274f371447f954eabdb49026
Youngwoo <zestlee1106@gmail.com>
2023-01-25 11:41:13 +0900

    [feat] commit lint 수정
    - stage 에 올라간 파일만 검색하도록 수정

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ['@nuxtjs/tailwindcss'],
 
-  css: ["@/assets/css/main.css"],
-});
+  css: ['@/assets/css/main.css'],
+})
