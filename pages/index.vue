0373ecdeae1084a50454b4a10e5f6e9f5760c7fc
Youngwoo <zestlee1106@gmail.com>
2023-04-04 18:05:16 +0900

    [test-1] 일단 한번에 커밋해 버린다 (미래의 나에게 미리 죄송)

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,13 @@
-<script>
-export default {
-  setup() {
-    return {}
-  },
-}
+<script setup lang="ts">
+import { BaseButton } from 'web-component-ywlee'
 </script>
 
 <template>
   <div class="flex justify-center items-center h-screen">
-    <h1 class="text-3xl font-bold text-green-500">
-      How to install Tailwind CSS 3 in Nuxt 3
-    </h1>
+    <h1 class="text-3xl font-bold text-green-500">How to install Tailwind CSS 3 in Nuxt 3</h1>
+    <no-ssr>
+      <BaseButton></BaseButton>
+    </no-ssr>
   </div>
 </template>
 
